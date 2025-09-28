35a0c17ded7111f5696858104613b74bdd8d6cd2
IamNayanIslam <iamnayanislam@gmail.com>
2025-09-28 21:49:50 +0600

    find intersection of arrays problem solved

--- app.js
+++ app.js
@@ -17,3 +17,24 @@ const moveByK = (arr, k) => {
 };
 
 console.log(moveByK([1, 2, 3, 4, 5], 5));
+
+const findArrayIntersection = (arr1, arr2) => {
+  const set1 = new Set(arr1);
+  const intersection = new Set();
+
+  for (const element of arr2) {
+    if (set1.has(element)) {
+      intersection.add(element);
+    }
+  }
+  return Array.from(intersection);
+};
+
+const arrayA = [1, 4, 9, 16, 25, 4];
+const arrayB = [9, 5, 4, 1, 1, 2];
+
+const commonElements = findArrayIntersection(arrayA, arrayB);
+
+console.log(`Array A: ${arrayA}`);
+console.log(`Array B: ${arrayB}`);
+console.log(`Intersection (Unique): ${commonElements}`); // Output: [9, 4, 1]
